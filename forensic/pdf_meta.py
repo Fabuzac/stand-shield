85003348b1d25ea99731e5e606b0966d352e6683
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-08-17 22:02:25 +0200

    Forensic tool improved

--- forensic/pdf_meta.py
+++ forensic/pdf_meta.py
@@ -1,11 +1,22 @@
 #!/user/bin/env python3
 # coding:utf8
-
 import PyPDF2
+import argparse
 
-pdf_file = PyPDF2.PdfFileReader(open("pdf/ANONOPS_The_Press_Release.pdf", "rb"))
+def get_pdf_meta(file_name):
+    pdf_file = PyPDF2.PdfFileReader(open(file_name, "rb"))
+    doc_info = pdf_file.getDocumentInfo()
 
-doc_info = pdf_file.getDocumentInfo()
+    for info in doc_info:
+        print("[+] " + info + " " + doc_info[info])
 
-for info in doc_info:
-    print("[+] " + info + " " + doc_info[info])
+# def get_string(file_name):
+#     with open(file_name, "rb") as file:
+#         content = file.read()
+
+parser = argparse.ArgumentParser(description="Outil de forensique")
+parser.add_argument("-pdf", dest="pdf", help="Chemin du fichier PDF", required=False)
+args = parser.parse_args()
+
+if args.pdf:
+    get_pdf_meta(args.pdf)
